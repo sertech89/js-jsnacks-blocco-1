72e0abffc7c4ba11c2209b6b0b892b7829f779fc
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-03-28 17:07:21 +0200

    snack 2

---
+++ snack 2/asset/main.js
@@ -0,0 +1,13 @@
+let par1;
+let par2;
+
+// while ((par1.length < -1)||(par2.length < -1) ){
+//     par1 = prompt(`Inserisci parola 1`);
+//     par2 = prompt(`Inserisci parola 2`);
+// }
+
+if (par1.lenght >= par2.lenght){
+    console.log(par2 + " " + par1)
+} else {
+    console.log(par1 + " " + par2)
+}
